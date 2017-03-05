6d14371dfe9807dc0f024d22e02cf5a6da40a072
pablogarces16 <pablogarcesee@gmail.com>
2017-03-04 20:13:56 -0800

    Added Ionic Files NEW

---
+++ src/pages/about/about.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+import {ArticlesPage} from '../articles/articles';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Component({
+  selector: 'page-about',
+  templateUrl: 'about.html'
+})
+export class AboutPage {
+
+  articlesPage = ArticlesPage;
+
+  posts: any;
+
+
+  constructor(public http: Http) {
+    this.http.get('https://stemy-160517.appspot.com/getNews').map(res => res.json()).subscribe(data => {
+      this.posts = data.children;
+      console.log(this.posts);
+    });
+  }
+
+}
